3c4c8afe3f0cc2dabf9f7575fa1de768d2892f40
Vibronic <andrey.brester@yandex.ru>
2020-10-21 21:23:50 +0300

    фрактальная пирамида отрисовка методом многочисленных итераций

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,19 @@
             this.components = new System.ComponentModel.Container();
             this.canvas = new System.Windows.Forms.PictureBox();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label_Cx = new System.Windows.Forms.TextBox();
+            this.label_Bx = new System.Windows.Forms.TextBox();
+            this.label_Ax = new System.Windows.Forms.TextBox();
+            this.Bx = new System.Windows.Forms.Label();
+            this.Ax = new System.Windows.Forms.Label();
+            this.Cx = new System.Windows.Forms.Label();
+            this.Enter = new System.Windows.Forms.Button();
+            this.Cy = new System.Windows.Forms.Label();
+            this.ay = new System.Windows.Forms.Label();
+            this.By = new System.Windows.Forms.Label();
+            this.label_Cy = new System.Windows.Forms.TextBox();
+            this.label_By = new System.Windows.Forms.TextBox();
+            this.label_Ay = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,11 +61,136 @@
             this.gameTimer.Interval = 16;
             this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label_Cx
+            // 
+            this.label_Cx.Location = new System.Drawing.Point(315, 25);
+            this.label_Cx.Name = "label_Cx";
+            this.label_Cx.Size = new System.Drawing.Size(77, 20);
+            this.label_Cx.TabIndex = 16;
+            this.label_Cx.TextChanged += new System.EventHandler(this.label_Cx_TextChanged);
+            // 
+            // label_Bx
+            // 
+            this.label_Bx.Location = new System.Drawing.Point(185, 25);
+            this.label_Bx.Name = "label_Bx";
+            this.label_Bx.Size = new System.Drawing.Size(74, 20);
+            this.label_Bx.TabIndex = 15;
+            this.label_Bx.TextChanged += new System.EventHandler(this.label_Bx_TextChanged);
+            // 
+            // label_Ax
+            // 
+            this.label_Ax.Location = new System.Drawing.Point(67, 25);
+            this.label_Ax.Name = "label_Ax";
+            this.label_Ax.Size = new System.Drawing.Size(73, 20);
+            this.label_Ax.TabIndex = 14;
+            this.label_Ax.TextChanged += new System.EventHandler(this.label_Ax_TextChanged);
+            // 
+            // Bx
+            // 
+            this.Bx.AutoSize = true;
+            this.Bx.Location = new System.Drawing.Point(148, 28);
+            this.Bx.Name = "Bx";
+            this.Bx.Size = new System.Drawing.Size(31, 13);
+            this.Bx.TabIndex = 4;
+            this.Bx.Text = "B(x)=";
+            // 
+            // Ax
+            // 
+            this.Ax.AutoSize = true;
+            this.Ax.Location = new System.Drawing.Point(30, 28);
+            this.Ax.Name = "Ax";
+            this.Ax.Size = new System.Drawing.Size(31, 13);
+            this.Ax.TabIndex = 5;
+            this.Ax.Text = "A(x)=";
+            // 
+            // Cx
+            // 
+            this.Cx.AutoSize = true;
+            this.Cx.Location = new System.Drawing.Point(278, 28);
+            this.Cx.Name = "Cx";
+            this.Cx.Size = new System.Drawing.Size(31, 13);
+            this.Cx.TabIndex = 6;
+            this.Cx.Text = "C(x)=";
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(398, 43);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(75, 23);
+            this.Enter.TabIndex = 7;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // Cy
+            // 
+            this.Cy.AutoSize = true;
+            this.Cy.Location = new System.Drawing.Point(278, 65);
+            this.Cy.Name = "Cy";
+            this.Cy.Size = new System.Drawing.Size(31, 13);
+            this.Cy.TabIndex = 13;
+            this.Cy.Text = "C(y)=";
+            // 
+            // ay
+            // 
+            this.ay.AutoSize = true;
+            this.ay.Location = new System.Drawing.Point(30, 65);
+            this.ay.Name = "ay";
+            this.ay.Size = new System.Drawing.Size(31, 13);
+            this.ay.TabIndex = 12;
+            this.ay.Text = "A(y)=";
+            // 
+            // By
+            // 
+            this.By.AutoSize = true;
+            this.By.Location = new System.Drawing.Point(148, 66);
+            this.By.Name = "By";
+            this.By.Size = new System.Drawing.Size(31, 13);
+            this.By.TabIndex = 11;
+            this.By.Text = "B(y)=";
+            // 
+            // label_Cy
+            // 
+            this.label_Cy.Location = new System.Drawing.Point(315, 62);
+            this.label_Cy.Name = "label_Cy";
+            this.label_Cy.Size = new System.Drawing.Size(77, 20);
+            this.label_Cy.TabIndex = 10;
+            this.label_Cy.TextChanged += new System.EventHandler(this.label_Cy_TextChanged);
+            // 
+            // label_By
+            // 
+            this.label_By.Location = new System.Drawing.Point(185, 63);
+            this.label_By.Name = "label_By";
+            this.label_By.Size = new System.Drawing.Size(74, 20);
+            this.label_By.TabIndex = 9;
+            this.label_By.TextChanged += new System.EventHandler(this.label_By_TextChanged);
+            // 
+            // label_Ay
+            // 
+            this.label_Ay.Location = new System.Drawing.Point(67, 63);
+            this.label_Ay.Name = "label_Ay";
+            this.label_Ay.Size = new System.Drawing.Size(73, 20);
+            this.label_Ay.TabIndex = 8;
+            this.label_Ay.TextChanged += new System.EventHandler(this.label_Ay_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.Cy);
+            this.Controls.Add(this.ay);
+            this.Controls.Add(this.By);
+            this.Controls.Add(this.label_Cy);
+            this.Controls.Add(this.label_By);
+            this.Controls.Add(this.label_Ay);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.Cx);
+            this.Controls.Add(this.Ax);
+            this.Controls.Add(this.Bx);
+            this.Controls.Add(this.label_Ax);
+            this.Controls.Add(this.label_Bx);
+            this.Controls.Add(this.label_Cx);
             this.Controls.Add(this.canvas);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -60,13 +198,27 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox canvas;
-        public System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.TextBox label_Cx;
+        private System.Windows.Forms.TextBox label_Bx;
+        private System.Windows.Forms.TextBox label_Ax;
+        private System.Windows.Forms.Label Bx;
+        private System.Windows.Forms.Label Ax;
+        private System.Windows.Forms.Label Cx;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.Label Cy;
+        private System.Windows.Forms.Label ay;
+        private System.Windows.Forms.Label By;
+        private System.Windows.Forms.TextBox label_Cy;
+        private System.Windows.Forms.TextBox label_By;
+        private System.Windows.Forms.TextBox label_Ay;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,44 +8,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Reflection;
 namespace First_try_of_game
 {
 
-    public class Player
-    {
-        public PointF pos;
-        public PointF acc;
-        public float size = 30f;
-        
-        public Player(PointF pos)
-        {
-
-        }
-    }
+  
    
     public partial class Form1 : Form
     {
         //создание переменных
         Bitmap scene;
-        int gametimer = 0;
         Graphics game;
-        Player player;
+        Player start;
         Brush blackbrush;
-
-
+        Pen blackpen;
+        PointF now;
         public Form1()
         {
             InitializeComponent();
             //инициализация
 
-            player = new Player(new PointF(player.size,scene.Height/2 + player.size));
+            
             scene = new Bitmap(canvas.Width, canvas.Height);
             game = Graphics.FromImage(scene);
-            gameTimer.Start();
+            
+            blackpen = new Pen(Color.Black);
             blackbrush = new SolidBrush(Color.Black);
-
-
+            start = new Player(new PointF(0,0),new PointF(0,0),new PointF(0,0));
+            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -60,17 +50,153 @@ namespace First_try_of_game
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            game.FillEllipse(
-                blackbrush,
-                player.pos.X - player.size / 2f,
-                player.pos.Y - player.size / 2f,
-                player.size,
-                player.size
-            );
-            game.Clear(Color.White);
+            var rand = new Random();
+
+            //game.Clear(Color.White);
+            //game.FillEllipse(blackbrush, player.A.X , player.A.Y , player.size, player.size);
+            //game.FillEllipse(blackbrush, player.B.X , player.B.Y , player.size, player.size);
+            //game.FillEllipse(blackbrush, player.C.X , player.C.Y , player.size, player.size);
+            // game.DrawLine(,player.pos.X, player.pos.Y, player.pos.X, player.pos.Y);
+            switch ( rand.Next(0, 3))
+            {
+                case 0:
+                    now.X += (0.5f * (start.A.X-now.X));
+                    now.Y += (0.5f * (start.A.Y-now.Y));
+                    break;
+                case 1:
+                    now.X += (0.5f * (start.B.X - now.X));
+                    now.Y += (0.5f * (start.B.Y - now.Y));
+                    break;
+                case 2:
+                    now.X += (0.5f * (start.C.X - now.X));
+                    now.Y += (0.5f * (start.C.Y - now.Y));
+                    break; }
+            game.FillEllipse(blackbrush, now.X, now.Y, 5,5);
             canvas.Image = scene;
-            gametimer++;
             
+            
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            if ( (start.A.X>0)&&(start.A.X < scene.Width) &&
+                (start.A.Y > 0) && (start.A.Y < scene.Width) &&
+                (start.B.X > 0) && (start.B.X < scene.Width) &&
+                (start.B.Y > 0) && (start.B.Y < scene.Width) &&
+                (start.C.X > 0) && (start.C.X < scene.Width) &&
+                (start.C.Y > 0) && (start.C.Y < scene.Width) )
+            {
+                label_Ax.Enabled = false;
+                label_Bx.Enabled = false;
+                label_Cx.Enabled = false;
+                label_Ay.Enabled = false;
+                label_By.Enabled = false;
+                label_Cy.Enabled = false;
+                Enter.Enabled = false;
+                gameTimer.Start();
+                game.FillEllipse(blackbrush, start.A.X, start.A.Y, start.size, start.size);
+                game.FillEllipse(blackbrush, start.B.X, start.B.Y, start.size, start.size);
+                game.FillEllipse(blackbrush, start.C.X, start.C.Y, start.size, start.size);
+                now = start.A;
+            }
+            
+           
+        }
+
+        public int stringToInt(string c)
+        {
+            int x=0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if ((c[i]=='0')|| (c[i] == '1') || (c[i] == '2') || (c[i] == '3') || (c[i] == '4') || (c[i] == '5') || (c[i] == '6') || (c[i] == '7') || (c[i] == '8') || (c[i] == '9'))
+                {
+                    switch (c[i])
+                    {
+                        case '0':
+                            x = x * 10 + 0;
+                            break;
+                        case '1':
+                            x = x * 10 + 1;
+                            break;
+                        case '2':
+                            x = x * 10 + 2;
+                            break;
+                        case '3':
+                            x = x * 10 + 3;
+                            break;
+                        case '4':
+                            x = x * 10 + 4;
+                            break;
+                        case '5':
+                            x = x * 10 + 5;
+                            break;
+                        case '6':
+                            x = x * 10 + 6;
+                            break;
+                        case '7':
+                            x = x * 10 + 7;
+                            break;
+                        case '8':
+                            x = x * 10 + 8;
+                            break;
+                        case '9':
+                            x = x * 10 + 9;
+                            break;
+                        
+                    }
+                }
+                else { return -1; }
+            }
+            
+            return x;
+        }
+
+        private void label_Ax_TextChanged(object sender, EventArgs e)
+        {
+
+            if (stringToInt(label_Ax.Text) == -1) { label_Ax.Text = "Это не цифра"; } else { start.A.X = stringToInt(label_Ax.Text); }
+        }
+
+        private void label_Bx_TextChanged(object sender, EventArgs e)
+        {
+            if (stringToInt(label_Bx.Text) == -1) { label_Bx.Text = "Это не цифра"; } else { start.B.X = stringToInt(label_Bx.Text); }
+        }
+
+        private void label_Cx_TextChanged(object sender, EventArgs e)
+        {
+            if (stringToInt(label_Cx.Text) == -1) { label_Cx.Text = "Это не цифра"; } else { start.C.X = stringToInt(label_Cx.Text); }
+        }
+
+        private void label_Cy_TextChanged(object sender, EventArgs e)
+        {
+            if (stringToInt(label_Cy.Text) == -1) { label_Cy.Text = "Это не цифра"; } else { start.C.Y = stringToInt(label_Cy.Text); }
+        }
+
+        private void label_By_TextChanged(object sender, EventArgs e)
+        {
+            if (stringToInt(label_By.Text) == -1) { label_By.Text = "Это не цифра"; } else { start.B.Y = stringToInt(label_By.Text); }
+        }
+
+        private void label_Ay_TextChanged(object sender, EventArgs e)
+        {
+            if (stringToInt(label_Ay.Text) == -1) { label_Ay.Text = "Это не цифра"; } else { start.A.Y = stringToInt(label_Ay.Text); }
+        }
+    }
+    
+    public class Player
+    {
+        public PointF A;
+        public PointF B;
+        public PointF C;
+        public PointF acc;
+        public float size = 30f;
+
+        public Player(PointF A, PointF B, PointF C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.acc = new PointF(0, 0);
         }
     }
 }
